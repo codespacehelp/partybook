3ea97e40feb1413d29b2faedddf1c939d2c75a34
Frederik De Bleser <frederik@debleser.be>
2025-05-07 11:00:25 +0200

    Add initial items and move them

--- party/index.js
+++ party/index.js
@@ -7,6 +7,30 @@
 /** @typedef {import("partykit/server").Connection} Connection */
 /** @typedef {import("partykit/server").ConnectionContext} ConnectionContext */
 
+const DEFAULT_BOOK_ITEMS = [
+  {
+    id: "abc123",
+    type: "image",
+    url: "https://6fsz3xa0qw.ufs.sh/f/obk1n6xKKhmuob4mcD2KKhmubZUGIXl9zFH6We5SR8kdysCg",
+    x: 150,
+    y: 150,
+  },
+  {
+    id: "cdb23",
+    type: "image",
+    url: "https://6fsz3xa0qw.ufs.sh/f/obk1n6xKKhmufaS2r942bs5vPCWA7xTnqNljHgkYmdZcByK8",
+    x: 200,
+    y: 200,
+  },
+  {
+    id: "def987",
+    type: "image",
+    url: "https://6fsz3xa0qw.ufs.sh/f/obk1n6xKKhmuW3QdLCrH6RA8bTiLjEPzeq2rfoNhdvOkXyIc",
+    x: 500,
+    y: 500,
+  },
+];
+
 /**
  * @implements {Server}
  */
@@ -19,21 +43,20 @@ class PartyServer {
     this.room = room;
   }
 
+  async onStart() {
+    this.items = (await this.room.storage.get("items")) ?? DEFAULT_BOOK_ITEMS;
+  }
+
   /**
    * @param {Connection} conn - The connection object.
    * @param {ConnectionContext} ctx - The context object.
    */
   onConnect(conn, ctx) {
     // A websocket just connected!
-    console.log(
-      `Connected:
-  id: ${conn.id}
-  room: ${this.room.id}
-  url: ${new URL(ctx.request.url).pathname}`,
-    );
+    console.log(`Connected: id: ${conn.id} room: ${this.room.id}`);
 
     // Send a message to the connection
-    // conn.send("hello from server");
+    conn.send(JSON.stringify({ type: "initial_items", id: conn.id, items: this.items }));
 
     // this.room.broadcast(`${conn.id} has connected`);
     this.room.broadcast(JSON.stringify({ type: "connect", id: conn.id }));
@@ -52,11 +75,14 @@ class PartyServer {
    * @param {Connection} sender
    */
   onMessage(message, sender) {
-    console.log(`connection ${sender.id} sent message: ${message}`);
+    // console.log(`connection ${sender.id} sent message: ${message}`);
     // Broadcast the received message to all other connections in the room except the sender
     const data = JSON.parse(message);
-    this.room.broadcast(JSON.stringify(data));
-    // this.room.broadcast(JSON.stringify(data), [sender.id]);
+    if (data.type !== "cursor") {
+      console.log(data);
+    }
+    // this.room.broadcast(JSON.stringify(data));
+    this.room.broadcast(JSON.stringify(data), [sender.id]);
   }
 }
 
--- static/test.js
+++ static/test.js
@@ -1,6 +1,9 @@
 import PartySocket from "https://esm.sh/partysocket";
 
 const cursors = new Map();
+const itemMap = new Map();
+const svgEl = document.querySelector("svg#canvas");
+let activeItemId = null;
 
 const ws = new PartySocket({
   // host: "project-name.username.partykit.dev",
@@ -22,10 +25,19 @@ ws.addEventListener("message", (e) => {
     document.querySelector("#cursors").removeChild(cursorEl);
     cursors.delete(message.id);
   } else if (message.type === "cursor") {
-    console.log(message);
+    // console.log(message);
     const cursorEl = cursors.get(message.id);
     cursorEl.style.left = `${message.x}px`;
     cursorEl.style.top = `${message.y}px`;
+  } else if (message.type === "initial_items") {
+    createOrUpdateItems(message.items);
+  } else if (message.type === "item_move") {
+    console.log(message);
+    const itemEl = itemMap.get(message.itemId);
+    itemEl.setAttribute("x", message.x);
+    itemEl.setAttribute("y", message.y);
+
+    // createOrUpdateItems(message.items);
   }
 });
 
@@ -36,7 +48,48 @@ document.querySelector("#test-send").addEventListener("click", () => {
 });
 
 window.addEventListener("mousemove", (e) => {
-  ws.send(
-    JSON.stringify({ type: "cursor", id: ws.id, x: e.clientX, y: e.clientY }),
-  );
+  ws.send(JSON.stringify({ type: "cursor", id: ws.id, x: e.clientX, y: e.clientY }));
 });
+
+function createOrUpdateItems(items) {
+  svgEl.innerHTML = "";
+  for (const item of items) {
+    if (item.type === "image") {
+      const imageEl = document.createElementNS("http://www.w3.org/2000/svg", "image");
+      imageEl.dataset.itemId = item.id;
+      imageEl.setAttribute("x", item.x);
+      imageEl.setAttribute("y", item.y);
+      imageEl.setAttribute("href", item.url);
+      // imageEl.setAttribute("transform", "scale(0.2)");
+      imageEl.addEventListener("mousedown", handleItemMouseDown);
+      itemMap.set(item.id, imageEl);
+      svgEl.appendChild(imageEl);
+    }
+  }
+}
+
+function handleItemMouseDown(e) {
+  // console.log(e);
+  activeItemId = e.target.dataset.itemId;
+
+  window.addEventListener("mousemove", handleItemMouseMove);
+  window.addEventListener("mouseup", handleItemMouseUp);
+}
+
+function handleItemMouseMove(e) {
+  const imageEl = itemMap.get(activeItemId);
+  console.log(imageEl.getAttribute("x"), imageEl.getAttribute("y"));
+  const oldX = parseInt(imageEl.getAttribute("x"));
+  const oldY = parseInt(imageEl.getAttribute("y"));
+  const newX = oldX + e.movementX;
+  const newY = oldY + e.movementY;
+  imageEl.setAttribute("x", newX);
+  imageEl.setAttribute("y", newY);
+  ws.send(JSON.stringify({ type: "item_move", id: ws.id, itemId: activeItemId, x: newX, y: newY }));
+}
+
+function handleItemMouseUp(e) {
+  window.removeEventListener("mousemove", handleItemMouseMove);
+  window.removeEventListener("mouseup", handleItemMouseUp);
+  activeItemId = null;
+}
